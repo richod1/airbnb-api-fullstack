074d029d1755e0a4b5c5c3624450c2fcde4d4389
richod@1 <frimpsup@gmail.com>
2023-01-05 18:19:40 -0800

    first commit

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+const port=3000;
+mongoose.set('strictQuery',false);
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+const routes = require('./routes/routes');
+
+app.use('/api', routes)
+
+app.listen(port, () => {
+    console.log(`Server Started at ${port}`)
+})
